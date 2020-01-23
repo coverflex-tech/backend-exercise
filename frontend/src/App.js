59d2421a5a6a9a6c86a6c288a4ad4bcf65915335
David Rego <davidrego@MacBook-Pro-de-David-2.local>
2020-01-23 15:41:58 +0000

    Create-react-app boilerplate

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import css from './App.module.css';
+
+function App() {
+  return (
+    <div className={css.host}>
+      Welcome to Bitcoin Exchange
+    </div>
+  );
+}
+
+export default App;
