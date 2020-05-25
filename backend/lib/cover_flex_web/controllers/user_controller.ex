c5762cc342555959c0e4f688c8290bf05cdb0166
neivars <raulsneiva@gmail.com>
2020-05-25 17:20:41 +0100

    Getting user now shows its product ids

--- backend/lib/cover_flex_web/controllers/user_controller.ex
+++ backend/lib/cover_flex_web/controllers/user_controller.ex
@@ -2,12 +2,14 @@ defmodule CoverFlexWeb.UserController do
 use CoverFlexWeb, :controller
 import Plug.Conn
 alias CoverFlex.Accounts
+alias CoverFlex.Products
 
   def show(conn, %{"id" => id}) do
     with user <- Accounts.ensure_user(id) do
+      products = Products.get_user_products(id)
       conn
       |> put_status(:ok)
-      |> render("show.json", %{user: user})
+      |> render("show.json", %{user: user, products: products})
     end
   end
 
--- backend/lib/cover_flex_web/views/user_view.ex
+++ backend/lib/cover_flex_web/views/user_view.ex
@@ -1,15 +1,15 @@
 defmodule CoverFlexWeb.UserView do
   use CoverFlexWeb, :view
 
-  def render("show.json", %{user: user}) do
-    %{user: render_one(user, CoverFlexWeb.UserView, "user.json")}
+  def render("show.json", %{user: user, products: products}) do
+    %{user: render_one(user, CoverFlexWeb.UserView, "user.json", products: products)}
   end
 
-  def render("user.json", %{user: user}) do
+  def render("user.json", %{user: user, products: products}) do
     %{user_id: user.id,
       data: %{
         balance: user.balance,
-        product_ids: []
+        product_ids: products
       }
     }
   end
