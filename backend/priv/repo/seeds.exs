2eb1ad77caeb8b216c6aa63bf6df25f5a2b0058e
Eduardo Cunha <eduardo.cunha@ist.utl.pt>
2021-02-24 12:32:51 +0000

    Update seeds script with a products list

--- backend/priv/repo/seeds.exs
+++ backend/priv/repo/seeds.exs
@@ -1,11 +1,11 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Backend.Repo.insert!(%Backend.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+Backend.Repo.insert_all(Backend.Product, [
+  %{id: "product_a", name: "Product A", price: 500},
+  %{id: "product_b", name: "Product B", price: 300},
+  %{id: "product_c", name: "Product C", price: 200},
+  %{id: "product_d", name: "Product D", price: 750},
+  %{id: "product_e", name: "Product E", price: 100},
+  %{id: "product_f", name: "Product F", price: 500},
+  %{id: "product_g", name: "Product G", price: 900},
+  %{id: "product_h", name: "Product H", price: 500},
+  %{id: "product_i", name: "Product I", price: 1500}
+])
