ca1cca5995e7f5bef6d13fd84b6ddd97043fe06a
Eduardo Cunha <eduardo.cunha@ist.utl.pt>
2021-02-24 16:50:17 +0000

    Add create/2 function to Order module
    This function creates an order for user purchased products and returns
    it.
    
    It checks if the given products exist or were already purchased by the
    user, or if the user has enough balance to purchase the products.

--- backend/lib/backend/order.ex
+++ backend/lib/backend/order.ex
@@ -1,8 +1,12 @@
 defmodule Backend.Order do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Ecto.Multi
+  alias Backend.User
 
   @primary_key {:order_id, :binary_id, autogenerate: true}
+
+  @type t :: %__MODULE__{}
 
   schema "orders" do
     embeds_one :data, Data, primary_key: false, on_replace: :update do
@@ -20,5 +24,77 @@ defmodule Backend.Order do
 
   defp data_changeset(data, attrs) do
     cast(data, attrs, [:user_id, :items, :total])
+  end
+
+  @doc """
+  Tries to create an order
+  """
+  @spec create(String.t(), [String.t()]) ::
+          {:ok, t()}
+          | {:error, :products_not_found | :products_already_purchased | :insufficient_balance}
+  def create(user_id, items) do
+    with {:ok, user} = Backend.User.get_or_create(user_id),
+         :ok <- already_purchased_products(user.data.product_ids, items),
+         products = Backend.Product.list(),
+         {:ok, filtered_products} <- check_and_filter_products(products, items),
+         {:ok, total, new_balance} <-
+           calculate_total_and_balance(user.data.balance, filtered_products) do
+      create_user_order(user, items, total, new_balance)
+    else
+      :already_purchased -> {:error, :products_already_purchased}
+      :product_not_found -> {:error, :products_not_found}
+      :insufficient_balance -> {:error, :insufficient_balance}
+    end
+  end
+
+  defp already_purchased_products(user_products, items) do
+    if Enum.any?(items, &Enum.member?(user_products, &1)) do
+      :already_purchased
+    else
+      :ok
+    end
+  end
+
+  defp check_and_filter_products(_products, []), do: :product_not_found
+
+  defp check_and_filter_products(products, items) do
+    filtered_products = Enum.filter(products, fn product -> product.id in items end)
+
+    if length(filtered_products) == length(items) do
+      {:ok, filtered_products}
+    else
+      :product_not_found
+    end
+  end
+
+  defp calculate_total_and_balance(user_balance, products) do
+    products
+    |> Enum.reduce_while({user_balance, 0}, fn product, {balance, total} ->
+      new_balance = balance - product.price
+
+      if new_balance < 0 do
+        {:halt, :insufficient_balance}
+      else
+        {:cont, {new_balance, total + product.price}}
+      end
+    end)
+    |> case do
+      {new_balance, total} -> {:ok, total, new_balance}
+      :insufficient_balance -> :insufficient_balance
+    end
+  end
+
+  defp create_user_order(user, items, total, new_balance) do
+    order_params = %{user_id: user.user_id, items: items, total: total}
+    user_params = %{balance: new_balance, product_ids: items ++ user.data.product_ids}
+
+    Multi.new()
+    |> Multi.insert(:order, __MODULE__.changeset(%__MODULE__{}, %{data: order_params}))
+    |> Multi.update(:user, User.changeset(user, %{data: user_params}))
+    |> Backend.Repo.transaction()
+    |> case do
+      {:ok, %{order: order}} -> {:ok, order}
+      err -> err
+    end
   end
 end
---
+++ backend/test/backend/order_test.exs
@@ -0,0 +1,52 @@
+defmodule Backend.OrderTest do
+  use Backend.DataCase, async: true
+  alias Backend.{Product, Order, User, Repo}
+
+  describe "create/2" do
+    test "returns {:ok, %Order{}} if it is able to create a user order" do
+      user_id = "user_id"
+      Repo.insert!(%User{user_id: user_id, data: %User.Data{}})
+
+      product_id_1 = "product_id_1"
+      product_id_2 = "product_id_2"
+
+      Repo.insert!(%Product{id: product_id_1, name: "Product 2", price: 10})
+      Repo.insert!(%Product{id: product_id_2, name: "Product 2", price: 10})
+
+      items = [product_id_1, product_id_2]
+
+      assert {:ok,
+              %Order{order_id: id, data: %Order.Data{user_id: ^user_id, total: 20, items: ^items}}} =
+               Order.create(user_id, items)
+
+      assert {:ok, _} = Ecto.UUID.cast(id)
+    end
+
+    test "returns {:error, :products_already_purchased} if the user already purchased the product" do
+      user_id = "user_id"
+      product_id = "product_id"
+
+      Repo.insert!(%User{user_id: user_id, data: %User.Data{product_ids: [product_id]}})
+
+      assert {:error, :products_already_purchased} = Order.create(user_id, [product_id])
+    end
+
+    test "returns {:error, :products_not_found} if a given items is an empty list" do
+      assert {:error, :products_not_found} = Order.create("user_id", [])
+    end
+
+    test "returns {:error, :products_not_found} if one of the given items does not exist" do
+      assert {:error, :products_not_found} = Order.create("user_id", ["inexisting_product"])
+    end
+
+    test "returns {:error, :insufficient_balance} if the user does not have enough balance to purchase a product" do
+      user_id = "user_id"
+      Repo.insert!(%User{user_id: user_id, data: %User.Data{balance: 1}})
+
+      product_id = "product_id"
+      Repo.insert!(%Product{id: product_id, name: "Product", price: 10})
+
+      assert {:error, :insufficient_balance} = Order.create(user_id, [product_id])
+    end
+  end
+end
