bea2e020aabab789d3e4352e5bfd1269930dbbe2
eltonplima <me@eltonplima.dev>
2021-06-18 06:51:11 +0100

    Cleanup

--- backend/apps/coverflex_web/lib/coverflex_web/controllers/order_controller.ex
+++ backend/apps/coverflex_web/lib/coverflex_web/controllers/order_controller.ex
@@ -5,10 +5,6 @@ defmodule CoverflexWeb.OrderController do
 
   action_fallback(CoverflexWeb.FallbackController)
 
-  #  def index(conn, _params) do
-  #    orders = Orders.list_orders()
-  #    render(conn, "index.json", orders: orders)
-  #  end
   def create(conn, %{"order" => %{"user_id" => user_id, "items" => products}}) do
     # TODO: Usar fallback para os errors: https://hexdocs.pm/phoenix/Phoenix.Controller.html#action_fallback/1
     case Orders.buy_products(user_id, products) do
@@ -38,25 +34,4 @@ defmodule CoverflexWeb.OrderController do
         |> render("400.json", [{:error, :insufficient_balance}])
     end
   end
-
-  #  def show(conn, %{"id" => id}) do
-  #    order = Orders.get_order!(id)
-  #    render(conn, "show.json", order: order)
-  #  end
-
-  #  def update(conn, %{"id" => id, "order" => order_params}) do
-  #    order = Orders.get_order!(id)
-  #
-  #    with {:ok, %Order{} = order} <- Orders.update_order(order, order_params) do
-  #      render(conn, "show.json", order: order)
-  #    end
-  #  end
-  #
-  #  def delete(conn, %{"id" => id}) do
-  #    order = Orders.get_order!(id)
-  #
-  #    with {:ok, %Order{}} <- Orders.delete_order(order) do
-  #      send_resp(conn, :no_content, "")
-  #    end
-  #  end
 end
