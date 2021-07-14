371bc130795e875d8665f05480a4b4248034d1e8
Ryan Dusmuir <ryan_dunsmuir@kansamples.com>
2021-07-14 15:58:41 +0100

    created seeds for products

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+p "Deleting everything"
+Product.destroy_all
+
+p "Creating products..."
+product_names = %w(Netflix Dental Masterclass Sushiclass Spotify Newshoes Cinemaclub Surfclass Gym Ubereats)
+product_names.each do |product_name|
+  Product.create(name: product_name, price: rand(10.00..200.00).round(2))
+end
+p "Created products!"
