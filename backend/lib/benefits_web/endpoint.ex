cbec6fc15370429bfc1e7be61640f4598e819505
vitorlimadev <vitorlimadev@outlook.com>
2021-12-19 05:06:22 -0300

    chore: add cors dep

--- backend/lib/benefits_web/endpoint.ex
+++ backend/lib/benefits_web/endpoint.ex
@@ -46,5 +46,6 @@ defmodule BenefitsWeb.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
   plug Plug.Session, @session_options
+  plug CORSPlug, origin: ["http://localhost:3000"]
   plug BenefitsWeb.Router
 end
--- backend/mix.exs
+++ backend/mix.exs
@@ -48,6 +48,7 @@ defmodule Benefits.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
+      {:cors_plug, "~> 2.0"},
       {:plug_cowboy, "~> 2.5"}
     ]
   end
