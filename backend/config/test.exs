c4bd9d2f12418528f7f20a92ed13b022d8e1373c
Cristian Manoel Pinheiro <cristian.m.pinheiro@gmail.com>
2022-01-27 19:01:39 -0300

    Initial backend commit

---
+++ backend/config/test.exs
@@ -0,0 +1,27 @@
+import Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :backend, Backend.Repo,
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  database: "backend_test#{System.get_env("MIX_TEST_PARTITION")}",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: 10
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :backend, BackendWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "G4ErYUiGXq9e7N3eY/6bgYwkyNIH8m5aT1ZA1E0Zq4I6gmDCzg549axP0vPCu3qM",
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
