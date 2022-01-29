0f359a96ad064ddabe5d9dd1951886b20d9f81a2
Bernardes <nuno.jmbernardes@gmail.com>
2022-01-29 15:47:50 +0000

    feat: seeds data to testing purposes

--- backend/priv/repo/seeds.exs
+++ backend/priv/repo/seeds.exs
@@ -9,3 +9,13 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias Benefits.Product
+alias Benefits.Repo
+alias Benefits.User
+
+Repo.insert!(%User{username: "admin", balance: 1000.0})
+Repo.insert!(%User{username: "bernardes", balance: 50.0})
+Repo.insert!(%Product{identifier: "netflix", name: "Netflix Subscription", price: 30.0})
+Repo.insert!(%Product{identifier: "spotify", name: "Spotify Subscription", price: 60.0})
+Repo.insert!(%Product{identifier: "amazon prime", name: "Amazon Prime Subscription", price: 10.0})
+Repo.insert!(%Product{identifier: "surf with Tiago", name: "Surf with Tiago", price: 0.0})
