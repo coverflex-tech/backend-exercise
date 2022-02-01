451a1cd9baa721ee1053097b8d70c59017370f53
Cristian Manoel Pinheiro <cristian.m.pinheiro@gmail.com>
2022-02-01 20:02:47 -0300

    test: add test to validate insufficient balance rule

--- backend/test/backend/orders_test.exs
+++ backend/test/backend/orders_test.exs
@@ -2,9 +2,10 @@ defmodule Backend.OrdersTest do
   use Backend.DataCase
 
   alias Backend.Orders
+  alias Backend.Orders.{Item, Order}
 
   describe "orders" do
-    import Backend.UsersFixtures
+    import Backend.{ProductsFixtures, UsersFixtures}
 
     test "create_order/2 with invalid user returns error" do
       assert {:error, "user_not_found"} = Orders.create_order(nil, [])
@@ -13,6 +14,12 @@ defmodule Backend.OrdersTest do
     test "create_order/2 with empty products list returns error" do
       user = user_fixture()
       assert {:error, "products_not_found"} = Orders.create_order(user, [])
+    end
+
+    test "create_order/2 with insufficient balance returns error" do
+      user = user_fixture(%{balance: 500})
+      products = [product_fixture(%{price: 300}), product_fixture(%{price: 300})]
+      assert {:error, "insufficient_balance"} = Orders.create_order(user, products)
     end
   end
 end
