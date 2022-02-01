63e8c61cb6eaac93d9ebad0b192e8ba15b4da17d
Cristian Manoel Pinheiro <cristian.m.pinheiro@gmail.com>
2022-02-01 20:10:18 -0300

    test: add test to validade products already purchased by user

--- backend/test/backend/orders_test.exs
+++ backend/test/backend/orders_test.exs
@@ -33,5 +33,13 @@ defmodule Backend.OrdersTest do
       products = [product_fixture(%{price: 300}), product_fixture(%{price: 300})]
       assert {:ok, {%Order{}, [%Item{}, %Item{}]}} = Orders.create_order(user, products)
     end
+
+    test "create_order/2 should not create order with previous ordered product by user" do
+      user = user_fixture(%{balance: 700})
+      products = [product_fixture(%{price: 300}), product_fixture(%{price: 300})]
+      assert {:ok, {%Order{}, [%Item{}, %Item{}]}} = Orders.create_order(user, products)
+
+      assert {:error, "products_already_purchased"} = Orders.create_order(user, products)
+    end
   end
 end
