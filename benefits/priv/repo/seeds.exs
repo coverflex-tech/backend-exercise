1320e2614b0e169ba1e0e4dbee97ca7c5ef7342d
Roger Souza <rogersec42@gmail.com>
2022-03-20 18:04:41 +0000

    seeds should only run on non-test env

--- benefits/priv/repo/seeds.exs
+++ benefits/priv/repo/seeds.exs
@@ -1,11 +1,13 @@
-alias Benefits.{Product, Repo}
+if Mix.env() != :test do
+  alias Benefits.{Product, Repo}
 
-now = NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second)
+  now = NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second)
 
-products_to_insert = [
-  %{name: "Netflix", price: Money.parse!("50.00"), inserted_at: now, updated_at: now},
-  %{name: "Iphone", price: Money.parse!("2500.00"), inserted_at: now, updated_at: now},
-  %{name: "Apple Watch", price: Money.parse!("500.50"), inserted_at: now, updated_at: now}
-]
+  products_to_insert = [
+    %{name: "Netflix", price: Money.parse!("50.00"), inserted_at: now, updated_at: now},
+    %{name: "Iphone", price: Money.parse!("2500.00"), inserted_at: now, updated_at: now},
+    %{name: "Apple Watch", price: Money.parse!("500.50"), inserted_at: now, updated_at: now}
+  ]
 
-Repo.insert_all(Product, products_to_insert)
+  Repo.insert_all(Product, products_to_insert)
+end
