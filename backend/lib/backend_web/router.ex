b8547d06c060cc7a0966f576169ca48fcb5b52d8
Ricardo Leão <ricardo@jaya.tech>
2022-03-21 20:34:09 -0300

    feat: initialize phoenix application

---
+++ backend/lib/backend_web/router.ex
@@ -0,0 +1,11 @@
+defmodule BackendWeb.Router do
+  use BackendWeb, :router
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/api", BackendWeb do
+    pipe_through :api
+  end
+end
