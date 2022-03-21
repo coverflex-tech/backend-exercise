a1aafd4de04ddc2d465d6fe6eed9970f7149da0d
mattheusmarzola <mattheusrml@gmail.com>
2022-03-21 12:35:48 -0300

    Added schema builders to tests

--- backend/test/support/builders.exs
+++ backend/test/support/builders.exs
@@ -3,17 +3,25 @@ defmodule Builders do
     quote do
       def create_user(opts \\ []) do
         params = Enum.into(opts, %{id: "frodo", balance: Decimal.new("100.00")})
-        Backend.Domain.Repositories.Users.create(params)
+
+        %Backend.Users.Schemas.User{}
+        |> Backend.Users.Schemas.User.changeset(params)
+        |> Backend.Repo.insert()
       end
 
       def create_order(opts \\ []) do
         params = Enum.into(opts, %{user_id: "frodo", total: Decimal.new("10.00")})
-        Backend.Domain.Repositories.Orders.create(params)
+
+        %Backend.Orders.Schemas.Order{}
+        |> Backend.Orders.Schemas.Order.changeset(params)
+        |> Backend.Repo.insert()
       end
 
       def create_product(opts \\ []) do
         params = Enum.into(opts, %{id: "netflix", name: "Netflix", price: Decimal.new("10.00")})
-        Backend.Domain.Repositories.Products.create(params)
+        %Backend.Products.Schemas.Product{}
+        |> Backend.Products.Schemas.Product.changeset(params)
+        |> Backend.Repo.insert()
       end
     end
   end
