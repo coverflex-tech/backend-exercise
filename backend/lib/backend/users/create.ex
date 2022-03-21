8e94377459deca57c3d6199d0176c2b3394379ff
mattheusmarzola <mattheusrml@gmail.com>
2022-03-21 13:12:28 -0300

    Fixed to user init with empty order_items

--- backend/lib/backend/users/create.ex
+++ backend/lib/backend/users/create.ex
@@ -5,9 +5,20 @@ defmodule Backend.Users.Create do
   @inital_balance Decimal.new("100.00")
 
   @spec call(%{
-          required(:id) => String.t(),
+          required(:id) => String.t()
         }) :: {:ok, User.t()} | {:error, Ecto.Changeset.t()}
   def call(%{id: user_id}) do
+    case create(user_id) do
+      {:ok, user} ->
+        user = Map.put(user, :order_items, [])
+        {:ok, user}
+
+      error ->
+        error
+    end
+  end
+
+  defp create(user_id) do
     %User{}
     |> User.changeset(%{id: user_id, balance: @inital_balance})
     |> Repo.insert()
