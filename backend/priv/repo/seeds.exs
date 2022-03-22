deaee32e9a1367527eeb20b4951b08cf50f5c385
mattheusmarzola <mattheusrml@gmail.com>
2022-03-21 22:03:22 -0300

    Added products seeds

--- backend/priv/repo/seeds.exs
+++ backend/priv/repo/seeds.exs
@@ -9,3 +9,22 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+[
+  %Backend.Products.Schemas.Product{
+    id: "netflix",
+    name: "Netflix",
+    price: Decimal.new("79.90")
+  },
+  %Backend.Products.Schemas.Product{
+    id: "spotify",
+    name: "Spotify",
+    price: Decimal.new("29.90")
+  },
+  %Backend.Products.Schemas.Product{
+    id: "amazon_prime_video",
+    name: "Amazon Prime Video",
+    price: Decimal.new("19.90")
+  }
+]
+|> Enum.each(&Backend.Repo.insert!/1)
