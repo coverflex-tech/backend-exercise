e71b0d6906c05bb5353ab7773e899e5d5508c472
Paulo A. S. Müller <paulomuller94@gmail.com>
2022-05-03 14:51:53 -0300

    chore: add credo dependency and config file

--- backend/mix.exs
+++ backend/mix.exs
@@ -43,7 +43,10 @@ defmodule Backend.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+
+      # code analysis
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
     ]
   end
 
